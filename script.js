159eb482721f4d22c081b429753be1b7ff512b33
careinicke <35277845+careinicke@users.noreply.github.com>
2018-03-09 14:18:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,89 +40,89 @@ $(document).ready(function () {
 // }); // sliding panels //
 
 
-$(document).ready(function () {
-	$("#a").click(function () {
+$(document).ready(function(){
+	$("#a").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 2 drug, medical use, highly addictive")
+		$("#popIn").text("Schedule 5 drug, medical use with lower potential for addiction and abuse.")
 	}) //start of drug grid//
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
 
-$(document).ready(function () {
-	$("#b").click(function () {
+$(document).ready(function(){
+	$("#b").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 1 drug, no medical use")
+		$("#popIn").text("Schedule 1 drug, no medical use and high potential for abuse.")
 	})
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
 
-$(document).ready(function () {
-	$("#c").click(function () {
+$(document).ready(function(){
+	$("#c").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 1 drug, no medical use. J caulkins 'If we balanced harm, liability against all the good fun people could have, we'd have a very different process.'")
-		$("#popIn").addClass("text_style"); //trying to add css to stuff on blue slide//
+		$("#popIn").text("Schedule 1 drug, no medical use and high potential for abuse. Dr. Caulkins said, 'If we balanced harm, liability against all the good fun people could have, we'd have a very different process.'")
+		$("#popIn").addClass("text_style"); //trying to add css to stuff on blue slide// 
 	})
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
 
-$(document).ready(function () {
-	$("#d").click(function () {
+$(document).ready(function(){
+	$("#d").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 1 drug, no medical use")
+		$("#popIn").text("Schedule 2 drug, medical use with a high potential for abuse and addiction. Use can potentially lead to severe psychological and physical dependence.")
 		$("#popIn").addClass("text_style");
 	})
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
 
-$(document).ready(function () {
-	$("#e").click(function () {
+$(document).ready(function(){
+	$("#e").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 1 drug, no medical use")
+		$("#popIn").text("Schedule 1 drug, no medical use and high potential for abuse.")
 		$("#popIn").addClass("text_style");
 	})
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
-$(document).ready(function () {
-	$("#f").click(function () {
+$(document).ready(function(){
+	$("#f").click(function(){
 		$("#slide").addClass('info_slides_show');
 		$("xButton").addClass("btn_close_show")
-		$("#popIn").text("Schedule 3 drug, medical use, addictive")
+		$("#popIn").text("Schedule 3 drug, some medical use, potential for addiction and abuse.")
 		$("#popIn").addClass("text_style");
 	})
 
-	$("#xButton").click(function () {
-		$("#slide").removeClass('info_slides_show');
-		$(".hotspot").removeClass('hide_btn');
-	});
+	$("#xButton").click(function(){
+ 	$("#slide").removeClass('info_slides_show');
+ 	$(".hotspot").removeClass('hide_btn');
+ });
 
 });
